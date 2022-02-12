2b18d88f69e6bc83b41142a98366619795172c1b
razvanpodar <razvan_podar@yahoo.com>
2022-02-12 16:23:37 +0200

    Backup

--- main.js
+++ main.js
@@ -1,35 +1,76 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, 
-  window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('.main'),
-});
+// const canvas = document.querySelector('.scene')
+// const scene = new THREE.Scene()
+// const camera = new THREE.PerspectiveCamera(75, 
+//     window.innerWidth / window.innerHeight, 0.1, 1000)
+// const renderer = new THREE.WebGLRenderer({
+//     canvas: canvas,
+// 	antialias: true,
+// })
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setPixelRatio(window.devicePixelRatio)
+// renderer.setSize(window.innerWidth, window.innerHeight)
 
-camera.position.setZ(30);
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+// controls.enableZoom = false
 
-renderer.render(scene, camera);
+// camera.position.setZ(100)
+// const light = new THREE.PointLight(0xff0000, 1, 100)
+// light.position.set(50, 50, 50)
+// scene.add(light)
 
-const geometry = new THREE.IcosahedronGeometry(10, 0);
-const material = new THREE.MeshBasicMaterial({ color: 0x76FF23 });
-const icosahedron = new THREE.Mesh(geometry, material);
+// const ambientLight = new THREE.AmbientLight(0x404040, 100)
+// scene.add(ambientLight)
+// renderer.render(scene, camera)
 
-scene.add(icosahedron);
+// // const loader = new GLTFLoader();
 
-function animate()
-{
-  requestAnimationFrame(animate);
+// // // loader.load('obj/cactus.glb', function(gltf) {
+// // //   const cactus = gltf.scene.children[0];
+// // //   cactus.scale.set(0.5, 0.5, 0.5);
+// // //   scene.add(gltf.scene);
+// // //   renderer.render(scene, camera);
+// // // });
 
-  icosahedron.rotation.x += 0.01;
-  icosahedron.rotation.y += 0.005;
-  icosahedron.rotation.z += 0.01;
+// const geometry = new THREE.IcosahedronGeometry(10, 0)
+// const material = new THREE.MeshBasicMaterial({ color: 0x76FF23 })
+// const icosahedron = new THREE.Mesh(geometry, material)
 
-  renderer.render(scene, camera);
-}
+// scene.add(icosahedron)
 
-animate();
+// // const sizes = {
+// //     width: window.innerWidth,
+// //     height: window.innerHeight
+// // }
+
+// // window.addEventListener('resize', () =>
+// // {
+// //     // Update sizes
+// //     sizes.width = window.innerWidth
+// //     sizes.height = window.innerHeight
+
+// //     // Update camera
+// //     camera.aspect = sizes.width / sizes.height
+// //     camera.updateProjectionMatrix()
+
+// //     // Update renderer
+// //     renderer.setSize(sizes.width, sizes.height)
+// //     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// // })
+
+// function animate()
+// {
+// 	requestAnimationFrame(animate)
+// 	controls.update()
+// 	icosahedron.rotation.x += 0.01
+// 	icosahedron.rotation.y += 0.005
+// 	icosahedron.rotation.z += 0.01
+// 	renderer.render(scene, camera)
+// }
+
+// animate()
